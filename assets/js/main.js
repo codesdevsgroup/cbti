756e464034a644809d4e30aa65d83f5f96917a92
Lucas Campos <bhlucascampos@gmail.com>
2023-09-18 14:02:50 -0300

    Adding back to top and WhatsApp buttons.

---
+++ assets/js/main.js
@@ -0,0 +1,37 @@
+﻿window.addEventListener('scroll', onScroll)
+
+onScroll()
+function onScroll() {
+  showNavOnScroll()
+  showBackTopButtonOnScroll()
+}
+
+function showNavOnScroll() {
+  if (window.scrollY > 100) {
+    $("#navigation").addClass("scroll")
+  } else {
+    $("#navigation").removeClass("scroll")
+  }
+}
+
+function showBackTopButtonOnScroll() {
+  if (window.scrollY > 500) {
+    $('#backToTopButton').show(100)
+  } else {
+    $('#backToTopButton').hide(100)
+  }
+}
+
+function showWhatsAppButtonOnScroll() {
+  if (window.scrollY > 500) {
+    $('.whatsapp-button').fadeIn(100)
+  } else {
+    $('.whatsapp-button').fadeOut(100)
+  }
+}
+
+$(window).scroll(function() {
+  showNavOnScroll()
+  showBackTopButtonOnScroll()
+  showWhatsAppButtonOnScroll()
+});
