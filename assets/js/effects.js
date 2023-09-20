a9e33ca45979ef7cc44dea091759c406623953ae
Lucas Campos <bhlucascampos@gmail.com>
2023-09-20 16:06:17 -0300

    insert lib ScrollReveal

---
+++ assets/js/effects.js
@@ -0,0 +1,97 @@
+window.sr = ScrollReveal({ reset: false })
+
+sr.reveal(
+  `
+  .scr_top
+`,
+  {
+    origin: 'top',
+    distance: '50px',
+    duration: 700,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_bottom
+`,
+  {
+    origin: 'bottom',
+    distance: '50px',
+    duration: 700,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_right
+`,
+  {
+    origin: 'right',
+    distance: '70px',
+    duration: 700,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_left
+`,
+  {
+    origin: 'left',
+    distance: '70px',
+    duration: 700,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_left_slow_1
+`,
+  {
+    origin: 'left',
+    distance: '70px',
+    duration: 900,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_left_slow_2
+`,
+  {
+    origin: 'left',
+    distance: '70px',
+    duration: 1100,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_left_slow_3
+`,
+  {
+    origin: 'left',
+    distance: '70px',
+    duration: 1300,
+    easing: 'ease'
+  }
+)
+
+sr.reveal(
+  `
+  .scr_top_slow_1
+`,
+  {
+    origin: 'top',
+    distance: '70px',
+    duration: 900,
+    easing: 'ease'
+  }
+)
